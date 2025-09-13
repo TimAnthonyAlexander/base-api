ad37fa94a6341d0aee518a94108a64321f6d87d1
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-09-13 20:58:38 +0200

    Do not rely on client type

--- src/Http/UploadedFile.php
+++ src/Http/UploadedFile.php
@@ -5,6 +5,7 @@ namespace BaseApi\Http;
 class UploadedFile
 {
     public string $name;
+    public string $clientType;
     public string $type;
     public string $tmpName;
     public int $error;
@@ -13,10 +14,11 @@ class UploadedFile
     public function __construct(array $fileData)
     {
         $this->name = $fileData['name'] ?? '';
-        $this->type = $fileData['type'] ?? '';
         $this->tmpName = $fileData['tmp_name'] ?? '';
         $this->error = $fileData['error'] ?? UPLOAD_ERR_NO_FILE;
         $this->size = $fileData['size'] ?? 0;
+        $this->clientType = $fileData['type'] ?? '';
+        $this->type = $this->getMimeType();
     }
 
     public function isValid(): bool
@@ -31,7 +33,8 @@ class UploadedFile
 
     public function getMimeType(): string
     {
-        return $this->type;
+        $f = new \finfo(FILEINFO_MIME_TYPE);
+        return $f->file($this->tmpName) ?: ($this->clientType ?? 'application/octet-stream');
     }
 
     public function getSize(): int
