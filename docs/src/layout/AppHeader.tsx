f2cd3e267fb6ad938e5911e8135f847c05d8ebfb
Tim Anthony Alexander <info@timanthonyalexander.de>
2025-09-21 18:10:12 +0200

    Update img in docs

--- docs/src/layout/AppHeader.tsx
+++ docs/src/layout/AppHeader.tsx
@@ -1,146 +1,159 @@
 import {
-  AppBar,
-  Toolbar,
-  Typography,
-  IconButton,
-  Box,
-  Select,
-  MenuItem,
-  FormControl,
-  Link as MuiLink,
+    AppBar,
+    Toolbar,
+    Typography,
+    IconButton,
+    Box,
+    Select,
+    MenuItem,
+    FormControl,
+    Link as MuiLink,
 } from '@mui/material';
 import {
-  Menu as MenuIcon,
-  DarkMode,
-  LightMode,
-  GitHub,
-  Forum as CommunityIcon,
+    Menu as MenuIcon,
+    DarkMode,
+    LightMode,
+    GitHub,
+    Forum as CommunityIcon,
 } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { VERSIONS, getCurrentVersion } from '../data/versions';
 import Search from './Search';
 
 interface AppHeaderProps {
-  drawerWidth: number;
-  onMenuClick: () => void;
-  darkMode: boolean;
-  onToggleDarkMode: () => void;
-  showMenuButton: boolean;
+    drawerWidth: number;
+    onMenuClick: () => void;
+    darkMode: boolean;
+    onToggleDarkMode: () => void;
+    showMenuButton: boolean;
 }
 
 export default function AppHeader({
-  drawerWidth,
-  onMenuClick,
-  darkMode,
-  onToggleDarkMode,
-  showMenuButton,
+    drawerWidth,
+    onMenuClick,
+    darkMode,
+    onToggleDarkMode,
+    showMenuButton,
 }: AppHeaderProps) {
-  const currentVersion = getCurrentVersion();
+    const currentVersion = getCurrentVersion();
 
-  return (
-    <AppBar
-      position="fixed"
-      sx={{
-        width: { lg: `calc(100% - ${drawerWidth}px)` },
-        ml: { lg: `${drawerWidth}px` },
-        backgroundColor: 'background.paper',
-        color: 'text.primary',
-        borderBottom: 1,
-        borderColor: 'divider',
-      }}
-      elevation={0}
-    >
-      <Toolbar>
-        {showMenuButton && (
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={onMenuClick}
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-        )}
-
-        <Typography
-          variant="h6"
-          noWrap
-          component={Link}
-          to="/"
-          sx={{
-            flexGrow: 0,
-            mr: 3,
-            textDecoration: 'none',
-            color: 'inherit',
-            fontWeight: 700,
-          }}
+    return (
+        <AppBar
+            position="fixed"
+            sx={{
+                width: { lg: `calc(100% - ${drawerWidth}px)` },
+                ml: { lg: `${drawerWidth}px` },
+                backgroundColor: 'background.paper',
+                color: 'text.primary',
+                borderBottom: 1,
+                borderColor: 'divider',
+            }}
+            elevation={0}
         >
-          BaseAPI
-        </Typography>
+            <Toolbar>
+                {showMenuButton && (
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={onMenuClick}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                )}
 
-        {/* Search */}
-        <Box sx={{ flexGrow: 1, maxWidth: 500, mx: 3 }}>
-          <Search />
-        </Box>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component={Link}
+                    to="/"
+                    sx={{
+                        flexGrow: 0,
+                        mr: 3,
+                        textDecoration: 'none',
+                        color: 'inherit',
+                        fontWeight: 700,
+                        justifyContent: 'center',
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <img
+                        src="/appstore.png"
+                        alt="Base API Logo"
+                        style={{
+                            height: 40,
+                            verticalAlign: 'middle',
+                            marginRight: 8,
+                            borderRadius: 8,
+                        }}
+                    />
+                    BaseAPI
+                </Typography>
 
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, ml: 'auto' }}>
-          {/* Version Selector */}
-          <FormControl size="small">
-            <Select
-              value={currentVersion.version}
-              variant="outlined"
-              sx={{
-                minWidth: 80,
-                '& .MuiOutlinedInput-notchedOutline': {
-                  border: 'none',
-                },
-                '&:hover .MuiOutlinedInput-notchedOutline': {
-                  border: '1px solid',
-                  borderColor: 'divider',
-                },
-              }}
-            >
-              {VERSIONS.map((version) => (
-                <MenuItem key={version.version} value={version.version}>
-                  {version.label}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+                {/* Search */}
+                <Box sx={{ flexGrow: 1, maxWidth: 500, mx: 3 }}>
+                    <Search />
+                </Box>
 
-          {/* Dark Mode Toggle */}
-          <IconButton
-            color="inherit"
-            onClick={onToggleDarkMode}
-            aria-label="toggle dark mode"
-          >
-            {darkMode ? <LightMode /> : <DarkMode />}
-          </IconButton>
+                <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, ml: 'auto' }}>
+                    {/* Version Selector */}
+                    <FormControl size="small">
+                        <Select
+                            value={currentVersion.version}
+                            variant="outlined"
+                            sx={{
+                                minWidth: 80,
+                                '& .MuiOutlinedInput-notchedOutline': {
+                                    border: 'none',
+                                },
+                                '&:hover .MuiOutlinedInput-notchedOutline': {
+                                    border: '1px solid',
+                                    borderColor: 'divider',
+                                },
+                            }}
+                        >
+                            {VERSIONS.map((version) => (
+                                <MenuItem key={version.version} value={version.version}>
+                                    {version.label}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
 
-          {/* Community Link */}
-          <IconButton
-            color="inherit"
-            component={Link}
-            to="/community"
-            aria-label="Community and support"
-          >
-            <CommunityIcon />
-          </IconButton>
+                    {/* Dark Mode Toggle */}
+                    <IconButton
+                        color="inherit"
+                        onClick={onToggleDarkMode}
+                        aria-label="toggle dark mode"
+                    >
+                        {darkMode ? <LightMode /> : <DarkMode />}
+                    </IconButton>
 
-          {/* GitHub Link */}
-          <IconButton
-            color="inherit"
-            component={MuiLink}
-            href="https://github.com/timanthonyalexander/base-api"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="GitHub repository"
-          >
-            <GitHub />
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
-  );
+                    {/* Community Link */}
+                    <IconButton
+                        color="inherit"
+                        component={Link}
+                        to="/community"
+                        aria-label="Community and support"
+                    >
+                        <CommunityIcon />
+                    </IconButton>
+
+                    {/* GitHub Link */}
+                    <IconButton
+                        color="inherit"
+                        component={MuiLink}
+                        href="https://github.com/timanthonyalexander/base-api"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="GitHub repository"
+                    >
+                        <GitHub />
+                    </IconButton>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
 }
